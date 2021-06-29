5312603c1132e288e9733f622c64d82c3b3c34df
minhyup <41357734+minhyup@users.noreply.github.com>
2021-06-29 10:41:22 +0900

    Stats preview 기본 작업 (#2)
    * 기본 마크업
    
    * test
    
    * s
    
    * pc대응
    
    * 100vh
    
    * 초기화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,16 @@ import type { AppProps } from 'next/app';
 
 const theme = {
   color: {
-    primary: 'red'
+    primary: {
+      mainBack: 'hsl(233, 47%, 7%)',
+      cardBack: 'hsl(244, 38%, 16%)',
+      soft: 'hsl(277, 64%, 61%)'
+    },
+    neutral: {
+      head: 'hsl(0, 0%, 100%)',
+      paragraph: 'hsla(0, 0%, 100%, 0.75)',
+      stat: 'hsla(0, 0%, 100%, 0.6)'
+    }
   }
 };
 
--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -5,8 +5,52 @@ export const GlobalStyles: React.FC = () => {
   return (
     <Global
       styles={css`
+        // reset
         body {
           margin: 0;
+          padding: 0;
+        }
+
+        // root
+        @font-face {
+          font-family: 'Inter';
+          font-weight: 400, 700;
+          src: url('https://fonts.google.com/specimen/Inter');
+        }
+        @font-face {
+          font-family: 'Lexend Deca';
+          font-weight: 400;
+          src: url('https://fonts.google.com/specimen/Lexend+Deca');
+        }
+
+        body {
+          font-size: 15px;
+          font-family: -apple-system, system-ui, HelveticaNeue, AppleSDGothicNeo, sans-serif;
+        }
+
+        h1,
+        h2,
+        h3,
+        p {
+          margin: 0;
+          padding: 0;
+        }
+        em {
+          font-style: normal;
+        }
+
+        ol,
+        ul,
+        li {
+          list-style: none;
+        }
+
+        fieldset,
+        img {
+          border: 0;
+        }
+        img {
+          max-width: 100%;
         }
       `}
     />
