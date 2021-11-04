5309a9d60a24f06fde1af03db492d6e16cd3a6b4
minhyup <minhyupp@gmail.com>
2021-11-04 15:41:53 +0900

    프로젝트초기화

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const Container = styled.div``;
+const Container = styled.div`
+  min-height: 100vh;
+  background-image: url(/images/pattern-background-desktop.svg);
+`;
 
 export default function MainPage(): React.ReactElement {
   return <Container>hello</Container>;
--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -6,10 +6,46 @@ export const GlobalStyles: React.FC = () => {
     <Global
       styles={css`
         // reset
-        body {
-          font-family: Red Hat Display, sans-serif;
+
+        // 1. 공백 제거
+        body,
+        p {
           margin: 0;
           padding: 0;
+        }
+
+        // 2.기본 폰트
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5 {
+          font-family: Red Hat Display, sans-serif;
+          font-weight: 500;
+          font-size: 16px;
+          /* line-height: 20px; */
+        }
+
+        // 3. 기본 바디 설정
+        body {
+          /* position: relative;
+          background-color: #fff;
+          font-size: 13px;
+          word-break: break-all; */
+        }
+
+        // 4. 기본 스타일 초기화
+        ol,
+        ul {
+          list-style: none;
+        }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        img {
+          vertical-align: top;
         }
 
         // root
